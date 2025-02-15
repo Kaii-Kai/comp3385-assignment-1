d8bfe31dd223e5fd54403d5f88ead55c72f93c49
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2025-02-15 00:24:04 -0400

    Modified file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
 	<h2>Kyra Lee</h2>
+	
+	<img src="{{url('/images/space.jpg')}}" alt="A purple/pink image of space" />
+	
 	<p>Hi, my name is Kyra Lee and I am a 19 year old student of The University Of The West Indies Five Islands Campus. 
 	Currently, I'm studying Computer Science, in hopes that I can one day become a Cyber Security Analyst. 
 	If I am not using my time to code new projects then you most likely would find me trying to cook new dishes or baking new treats </p>
-	
-	<img src="{{url('/images/space.jpg')}}" alt="A purple/pink image of space" />
 @endsection
