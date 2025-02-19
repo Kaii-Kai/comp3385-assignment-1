58a138b4c77661395d53cfd677076422e2882aac
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2025-02-18 23:08:57 -0400

    Modified file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,5 +7,5 @@
 	
 	<p>Hi, my name is Kyra Lee and I am a 19 year old student of The University Of The West Indies Five Islands Campus. 
 	Currently, I'm studying Computer Science, in hopes that I can one day become a Cyber Security Analyst. 
-	If I am not using my time to code new projects then you most likely would find me trying to cook new dishes or baking new treats </p>
+	If I am not using my time to code new projects then you most likely would find me trying to cook new dishes or baking new treats. </p>
 @endsection
